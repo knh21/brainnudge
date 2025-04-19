e5cac3a5414062a90ea9a4ee35c8311d709ed40c
kyle <kyle.hill56@yahoo.com>
2025-04-19 16:25:14 -0400

    starting to add js functions

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
-const nudgeText = document.getElementById("nudge-text")
-const doneButton = document.getElementById("done-btn")
-const afterMessage = document.getElementById("after-message")
+const nudgeButton = document.getElementById('nudge-btn')
+const nudgePrompt = document.getElementById('nudge-prompt')
+const doneButton = document.getElementById('done-btn')
 
 
+function getNudge(){
+    nudgeButton.classList.add('hidden')
+    nudgePrompt.classList.remove('hidden')
+    doneButton.classList.remove('hidden')
+}
+
+function doneNudge(){
+    nudgePrompt.classList.add('hidden') // Hide the nudge prompt
+    nudgeButton.classList.remove('hidden')  // Show the "Get Nudge" button
+    doneButton.classList.add('hidden') // Hide the "Done" button
+}
